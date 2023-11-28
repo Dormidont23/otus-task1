2f49eb034c95540d6995fa387f1d9b2a0e63628e
Dormidont23 <149655842+Dormidont23@users.noreply.github.com>
2023-11-28 16:00:17 +0700

    Add files via upload

---
+++ packer/scripts/stage-1-kernel-update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Установка репозитория elrepo
+sudo yum install -y https://www.elrepo.org/elrepo-release-7.el7.elrepo.noarch.rpm
+# Установка нового ядра из репозитория elrepo-kernel
+sudo yum --enablerepo elrepo-kernel install kernel-ml -y
+
+# Обновление параметров GRUB
+sudo grub2-set-default 0
+sudo grub2-mkconfig -o /boot/grub2/grub.cfg
+
+echo "Grub update done."
+# Перезагрузка ВМ
+shutdown -r now
